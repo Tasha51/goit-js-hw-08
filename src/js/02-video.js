99a4e87fe0f2d8fc889641e8bb57a2ef953e92c5
Nataliia Tatsiienko <natasha51star29@gmail.com>
2022-07-28 22:42:35 +0300

    add task №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+ const player = new Player(iframe);
+
+    player.on('timeupdate', throttle((time) => {
+        localStorage.setItem(LOCALSTORAGE_KEY, time.seconds);
+        console.log(time.seconds);
+    }, 1000)
+    );
+
+onload();
+
+function onload() {
+    player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY)); 
+};
