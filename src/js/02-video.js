0e4c9d55b9e67a6c975022c5a7292c3694be0dc8
Nataliia Tatsiienko <natasha51star29@gmail.com>
2022-08-01 00:00:59 +0300

    debugging

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,16 +4,21 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
- const player = new Player(iframe);
+const player = new Player(iframe);
 
-    player.on('timeupdate', throttle((time) => {
+
+player.on('timeupdate', throttle((time) => {
         localStorage.setItem(LOCALSTORAGE_KEY, time.seconds);
-        console.log(time.seconds);
+    console.log(time.seconds);
     }, 1000)
     );
 
 onload();
 
 function onload() {
-    player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY)); 
+    const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+    if (currentTime) {
+       player.setCurrentTime(currentTime); 
+    }
 };
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,33 +3,35 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
-let inputsText = {};
-
-
 const STORAGE_KEY = "feedback-form-state";
-
 
 initForm();
 
 form.addEventListener('input', throttle(saveMessage), 500);
 
-
 form.addEventListener("submit", clearInput);
 
-
-
 function saveMessage(event) {
-    inputsText[event.target.name] = event.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(inputsText));
-    
+
+    let parsistedInputsText = localStorage.getItem(STORAGE_KEY);
+    parsistedInputsText = parsistedInputsText ? JSON.parse(parsistedInputsText) : {};
+
+     parsistedInputsText[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify( parsistedInputsText));
 };
 
 
 function clearInput(event) {
     event.preventDefault();
+   
     const formData = new FormData(form);
-    formData.forEach((name, value) => console.log(name, value));
-    form.reset();
+    
+    const formDataObj = {};
+
+    formData.forEach((value, name) => (formDataObj[name] = value));
+    console.log(formDataObj);
+
+    event.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 };
 
@@ -37,10 +39,9 @@ function initForm() {
     let parsistedInputsText = localStorage.getItem(STORAGE_KEY);
     if (parsistedInputsText) {
         parsistedInputsText = JSON.parse(parsistedInputsText);
-        // console.log(parsistedInputsText);
         Object.entries(parsistedInputsText).forEach(([name, value]) => {
             form.elements[name].value = value;
-            inputsText[name] = value;
+            
         });
     };
    
